4941b605fce5dbfd871e6157d60a2f66923e4300
AndreCerqueira <andrecerq16@gmail.com>
2022-02-05 22:30:47 +0000

    fund me with brownie

---
+++ FundMe_Brownie/scripts/helpful_scripts.py
@@ -0,0 +1,28 @@
+from brownie import MockV3Aggregator, network, config, accounts
+from web3 import Web3
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+DECIMALS = 8
+STARTING_PRICE = 200000000000
+
+
+def get_account():
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mocks():
+    print(f"the active network is {network.show_active()}")
+    print("Deploying Mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
+        )
+    print("Mocks Deployed...")
